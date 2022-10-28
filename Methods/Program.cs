d3b0add5636f869c164e92443f5d1028f791baf1
Name2408 <bogdanevseyv@yandex.ru>
2022-10-28 16:06:53 +0300

    добавил папку методы

---
+++ Methods/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+string Method1 (int count, string text)
+{
+    string result = String.Empty; 
+
+    for (int i=0; i<count; i++)
+    {
+        result = result + text;
+    }
+
+    return result;
+}
+
+string res = Method1(100, "хаХа");
+Console.WriteLine(res);
